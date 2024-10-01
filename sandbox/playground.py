e68aa0b9cd5daced973b3d4dffce5666018a582a
theplatecrafter <theplatecrafter@gmail.com>
2024-10-01 21:03:08 +0900

    wip

--- sandbox/playground.py
+++ sandbox/playground.py
@@ -15,4 +15,4 @@ dh.view_dwg(dh.os.path.join(dh.dwg_output_dir,"out.dwg"),dh.image_output_dir)
 
 exit()
 
-## TODO: maybe change lwpolyline to polyline?
+## TODO: maybe change lwpolyline to polyline? for corel draw
