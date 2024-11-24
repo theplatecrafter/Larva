d1fea66ef34c31768ea516872ac31bd0f0759871
theplatecrafter <theplatecrafter@gmail.com>
2024-11-24 20:26:06 +0900

    wip

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, request, jsonify
 import os
-import trimesh
 import Larva.direct_data_handler as ddh
 
 app = Flask(__name__)
 
 @app.route('/slice-stl', methods=['POST'])
 def slice_stl():
+    print("got stuff!")
     stlFile = request.files['stlFile']
     normalX = float(request.form['normalX'])
     normalY = float(request.form['normalY'])
@@ -15,21 +15,18 @@ def slice_stl():
     pointY = float(request.form['pointY'])
     pointZ = float(request.form['pointZ'])
 
-    # Save uploaded file to temp directory
     file_location = f"temp/{stlFile.filename}"
     os.makedirs('temp', exist_ok=True)
     stlFile.save(file_location)
 
-    # Prepare slicing parameters
     slicing_plane_normal = [normalX, normalY, normalZ]
     slicing_plane_point = [pointX, pointY, pointZ]
+    print(slicing_plane_normal,slicing_plane_point)
 
-    # Define output directory and name
     output_dir = "output"
     output_name = "sliced_output"
     os.makedirs(output_dir, exist_ok=True)
     
-    # Call your slicing function
     ddh.DEVslice_stl(file_location, slicing_plane_normal, slicing_plane_point, output_dir, output_name)
 
     return jsonify({"message": f"File sliced and saved as {output_name}.dwg"})
